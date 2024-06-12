d23bdd5f247285b01f58014e493d48cbe9c8c1b1
*Lungile* <*lungile.simelane4@gmail.com*>
2024-06-12 11:22:33 +0200

    created datatype.js

---
+++ datatpye.js
@@ -0,0 +1,78 @@
+// Exercise 1: Numbers
+
+// Declare a number variable and assign it a value.
+let number1 = 10;
+
+// Declare a number variable and assign it a floating number.
+let number2 = 5.5;
+
+// Perform addition, subtraction, multiplication, division, modulus and exponentiation with number variable and another number.
+let addition = number1 + number2;
+let subtraction = number1 - number2;
+let multiplication = number1 * number2;
+let division = number1 / number2;
+let modulus = number1 % number2;
+let exponentiation = number1 ** number2;
+
+// Print all your variables.
+console.log(number1);
+console.log(number2);
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
+console.log(exponentiation);
+
+// Exercise 2: Boolean and Operators
+
+// Use comparison operators to compare two numbers and store the results in boolean variables.
+let compareNum1 = 15;
+let compareNum2 = 20;
+
+let isGreater = compareNum1 > compareNum2;
+let isLessOrEqual = compareNum1 <= compareNum2;
+let isEqual = compareNum1 === compareNum2;
+let isNotEqual = compareNum1 !== compareNum2;
+
+// Print the results of the comparisons.
+console.log(isGreater);
+console.log(isLessOrEqual);
+console.log(isEqual);
+console.log(isNotEqual);
+
+// Step 2: Declare two variables x and y with the values 8 and 12 respectively.
+let x = 8;
+let y = 12;
+
+// Use comparison operators to check and print the following.
+console.log(x > y);
+console.log(x <= y);
+console.log(x === y);
+console.log(x !== y);
+
+// Step 3: Declare the variables a and b with values true and false, determine and print the result of the following logical operations.
+let a = true;
+let b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+// Step 4: Declare variable p and assign it a value of 10. Use the following assignment operators to modify the value of p with any number and print the result each time.
+let p = 10;
+
+p += 5;
+console.log(p);
+
+p -= 3;
+console.log(p);
+
+p *= 2;
+console.log(p);
+
+p /= 4;
+console.log(p);
+
+p %= 3;
+console.log(p);
